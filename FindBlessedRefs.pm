3fb27fc1ae57b25965aac2a2bb4f08f32867ef17
Paul Miller <jettero@cpan.org>
2008-07-31 18:30:59 +0000

    Import of JETTERO/Devel-FindBlessedRefs-1.1.3 from CPAN.
    gitpan-cpan-distribution: Devel-FindBlessedRefs
    gitpan-cpan-version:      1.1.3
    gitpan-cpan-path:         JETTERO/Devel-FindBlessedRefs-1.1.3.tar.gz
    gitpan-cpan-author:       JETTERO
    gitpan-cpan-maturity:     released

--- FindBlessedRefs.pm
+++ FindBlessedRefs.pm
@@ -10,11 +10,9 @@ use base 'Exporter';
 our %EXPORT_TAGS = ( all => [qw( find_refs find_refs_by_coderef )]);
 our @EXPORT_OK   = ( @{$EXPORT_TAGS{all}} );
 
-use version; our $VERSION = qv('1.1.2');
+use version; our $VERSION = qv('1.1.3');
 
 require XSLoader;
 XSLoader::load('Devel::FindBlessedRefs', $VERSION);
 
 1;
-
-__END__
